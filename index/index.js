11bee2ea629f84294e32a7af83f21d52e9edf8cc
牛牛君 <1007503677@qq.com>
2024-09-30 18:23:01 +0800

    添加内容

--- index/index.js
+++ index/index.js
@@ -7,19 +7,6 @@ $(function () {
     $("#root").fullpage();
 });
 
-//标题检测显示
-window.onload = function () {
-    var originalTitle = document.title;
-    document.addEventListener('visibilitychange', function () {
-        if (document.visibilityState === 'visible') {
-            document.title = 'FAMS';    //在线
-        } else {
-            document.title = '人呢?( ´•.• ` )';     //离线
-        }
-    }
-    );
-};
-
 // 副标题轮换
 const subtitles = ["一个奇奇怪怪的服务器", "为斯卡蒂献上心脏", "博士,还不能休息哟!"];    // 这里添加副标题轮换内容
 let currentIndex = 0;
